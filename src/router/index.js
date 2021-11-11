d1fc732f893d0e69f603a79d473f5ee69b24c184
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-11-11 23:24:33 +0530

    #Initail commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-const routes = [
+const routerOptions = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    componentPath: 'Home'
   },
   {
     path: '/about',
@@ -13,12 +13,23 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    componentPath: 'About'
   }
-]
+];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const routes = routerOptions.map(route => {
+  return {
+      ...route,
+      component: () => import(`@/components/${route.componentPath}.vue`)
+  }
+})
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    runtimeCompiler: true,
+    productionSourceMap: false,
+    outputDir: 'dist',
+    assetsDir: 'static',
+    // publicPath: process.env.NODE_ENV === 'production'
+    // ? '/my-project/'
+    // : '/'
+}
